c99d4eb6d94ff8c82afa3de8e938d7f62fd2637d
Kashit <75305061+HeyKashit@users.noreply.github.com>
2022-05-23 19:08:37 +0530

    Create program2.c

---
+++ program2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    printf("enter the number 1 ");
+    scanf("%d",&a);
+    printf("enter the number 2 ");
+    scanf("%d",&b);
+    
+    printf("the ans is %d",a+b);
+    return 0;
+}
